dc517ff89e5e752328f9276646f0b42ef77b6426
Hang Vu <onion209.hust@gmail.com>
2024-11-21 20:06:10 +0700

    commit xàm nè

--- download_data.py
+++ download_data.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import kagglehub
 import pandas as pd
 # Download latest version
@@ -6,7 +7,10 @@ path = kagglehub.dataset_download("rish59/financial-statements-of-major-companie
 # print("Files in the directory:", os.listdir(path))
 # Specify the correct CSV file path
 file_path = os.path.join(path, "Financial Statements.csv")
+# Copy the file to a new location
+destination_path = "/home/minhlahanhne/finance_statement_LLMs/Financial Statements.csv"
+shutil.copy(file_path, destination_path)
 
 # Read the CSV file
 df = pd.read_csv(file_path)
-# print(df.info())
+print(df.info())
